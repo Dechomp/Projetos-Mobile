5af8e82e3d40728c87213ddd40ee5d80c03ea6a2
Dechomp <julio.sougosan@gmail.com>
2025-08-28 20:11:30 -0300

    Corrigindo o bug do gerador de senhas + começando o multTela

--- MultTela/app/src/main/java/com/example/multtela/MainActivity.java
+++ MultTela/app/src/main/java/com/example/multtela/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.multtela;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,25 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
-    Button btOK, btCancel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-
-        btOK = findViewById(R.id.btOK);
-        btCancel = findViewById(R.id.btCancel);
-
-        btOK.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
+        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
