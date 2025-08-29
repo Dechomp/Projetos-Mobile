99d4b2a8e5eb4a43eca723cddc70bd0665f2de9e
Dechomp <julio.sougosan@gmail.com>
2025-08-28 22:13:19 -0300

    CPF terminado, continuando mult tela

--- MultTela/app/src/main/java/com/example/multtela/LoginActivity.java
+++ MultTela/app/src/main/java/com/example/multtela/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.example.multtela;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +16,10 @@ import androidx.core.view.WindowInsetsCompat;
 public class LoginActivity extends AppCompatActivity {
 
     Button btOK, btCancel;
+
+    EditText edUser, edSenha;
+
+    TextView tvSituacao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +29,47 @@ public class LoginActivity extends AppCompatActivity {
         btOK = findViewById(R.id.btOK);
         btCancel = findViewById(R.id.btCancel);
 
+
+        edUser = findViewById(R.id.edUser);
+        edSenha = findViewById(R.id.edSenha);
+
+        tvSituacao = findViewById(R.id.tvSituacao);
         btOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String user;
 
+                user = edUser.getText().toString();
+                if (user != "Adm"){
+                    tvSituacao.setText("Usuário não encontrado");
+                }
+                else{
+
+                    String senha;
+
+                    senha = edSenha.getText().toString();
+
+                    if(senha.equals("123456")){
+                        Intent telaPrincipal;
+
+                        telaPrincipal = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(telaPrincipal);
+                    }
+
+                }
+
+            }
+        });
+
+        btCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvSituacao.setText("----------");
+                edUser.setText("");
+                edUser.setHint("Digite aqui...");
+
+                edSenha.setText("");
+                edSenha.setHint("Digite aqui...");
             }
         });
 
