23f37015a6ac2c81671393b550e52a66c8ccf05b
Dechomptile <juliodechomp@gmail.com>
2025-09-01 17:33:49 -0300

    Continunado

--- GamestudandoBeta/app/src/main/java/com/example/gamestudandobeta/SplashActivity.java
+++ GamestudandoBeta/app/src/main/java/com/example/gamestudandobeta/SplashActivity.java
@@ -10,23 +10,40 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
+
+
+        Handler h = new Handler();
+
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                exibirMainActivy();
+
+            }
+        }, 4000);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-
-
-
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
     }
 
+    private void exibirMainActivy(){
+        Intent telaInicial;
 
+        telaInicial = new Intent(SplashActivity.this, MainActivity.class);
+
+
+        startActivity(telaInicial);
+        finish();
+    }
 }
