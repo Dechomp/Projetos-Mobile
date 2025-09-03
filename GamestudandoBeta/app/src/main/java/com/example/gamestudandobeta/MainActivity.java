911294a8c4484844ff8061f72b87d4fab1ace919
Dechomptile <juliodechomp@gmail.com>
2025-09-03 17:23:56 -0300

    Iniciando os teste, colocando o scroll view

--- GamestudandoBeta/app/src/main/java/com/example/gamestudandobeta/MainActivity.java
+++ GamestudandoBeta/app/src/main/java/com/example/gamestudandobeta/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.gamestudandobeta;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +13,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btSQL, btJava, btAndroid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +22,43 @@ public class MainActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
 
 
+            btSQL = findViewById(R.id.btSQL);
+            btJava = findViewById(R.id.btJava);
+            btAndroid = findViewById(R.id.btAndroid);
 
+            btSQL.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent testeSQL;
+
+                    testeSQL = new Intent(MainActivity.this, TesteSqlActivity.class);
+
+                    startActivity(testeSQL);
+                }
+            });
+
+            btJava.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent testeJava;
+
+                    testeJava = new Intent(MainActivity.this, TesteJavaActivity.class);
+
+                    startActivity(testeJava);
+                }
+            });
+
+            btAndroid.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent testeAndroid;
+
+                    testeAndroid = new Intent(MainActivity.this, TesteAndroidActivity.class);
+
+
+                    startActivity(testeAndroid);
+                }
+            });
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
