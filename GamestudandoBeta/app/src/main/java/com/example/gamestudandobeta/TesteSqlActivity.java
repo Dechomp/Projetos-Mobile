03edaf5c01fb04a2bbbe317663e31e3b88d251a9
Dechomptile <juliodechomp@gmail.com>
2025-09-09 17:19:24 -0300

    Aleatorizando as perguntas

--- GamestudandoBeta/app/src/main/java/com/example/gamestudandobeta/TesteSqlActivity.java
+++ GamestudandoBeta/app/src/main/java/com/example/gamestudandobeta/TesteSqlActivity.java
@@ -1,6 +1,10 @@
 package com.example.gamestudandobeta;
 
+import static android.graphics.Color.*;
+
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,15 +19,22 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class TesteSqlActivity extends AppCompatActivity {
-    Button btQuestao1a, btQuestao1b, btQuestao1c, btQuestao1d, btVerificar;
+    Button btQuestao1a, btQuestao1b, btQuestao1c, btQuestao1d, btVerificar, btSelecionado;
 
     TextView tvQuestaoNum, tvQuestaoTexto;
 
     String respCerta;
 
     int numQuestao = 0;
+
+    ArrayList<String> linhas = new ArrayList<>();
+
+    int[] ordem = new int[30];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +56,133 @@ public class TesteSqlActivity extends AppCompatActivity {
 
         carregarPerguntas();
 
+        btQuestao1a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (btSelecionado != btQuestao1a){
+                    btSelecionado = btQuestao1a;
+
+                    btQuestao1a.setBackgroundColor(parseColor("#66e0ff"));
+                    btQuestao1b.setBackgroundColor(parseColor("#668cff"));
+                    btQuestao1c.setBackgroundColor(parseColor("#668cff"));
+                    btQuestao1d.setBackgroundColor(parseColor("#668cff"));
+
+                    btVerificar.setClickable(true);
+                    btVerificar.setBackgroundColor(parseColor("#bfff00"));
+
+                }
+                else{
+                    btQuestao1a.setBackgroundColor(parseColor("#668cff"));
+                    btSelecionado = null;
+                    btVerificar.setClickable(false);
+                    btVerificar.setBackgroundColor(parseColor("#8E8C8C"));
+                }
+
+
+            }
+        });
+
+        btQuestao1b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(btSelecionado != btQuestao1b){
+                    btSelecionado = btQuestao1b;
+
+                    btQuestao1a.setBackgroundColor(parseColor("#668cff"));
+                    btQuestao1b.setBackgroundColor(parseColor("#66e0ff"));
+                    btQuestao1c.setBackgroundColor(parseColor("#668cff"));
+                    btQuestao1d.setBackgroundColor(parseColor("#668cff"));
+
+                    btVerificar.setClickable(true);
+                    btVerificar.setBackgroundColor(parseColor("#bfff00"));
+                }
+                else{
+                    btQuestao1b.setBackgroundColor(parseColor("#668cff"));
+                    btSelecionado = null;
+                    btVerificar.setClickable(false);
+                    btVerificar.setBackgroundColor(parseColor("#8E8C8C"));
+                }
+
+            }
+        });
+
+        btQuestao1c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (btSelecionado != btQuestao1c){
+                    btSelecionado = btQuestao1c;
+
+                    btQuestao1a.setBackgroundColor(parseColor("#668cff"));
+                    btQuestao1b.setBackgroundColor(parseColor("#668cff"));
+                    btQuestao1c.setBackgroundColor(parseColor("#66e0ff"));
+                    btQuestao1d.setBackgroundColor(parseColor("#668cff"));
+
+                    btVerificar.setClickable(true);
+                    btVerificar.setBackgroundColor(parseColor("#bfff00"));
+                }
+                else{
+                    btQuestao1c.setBackgroundColor(parseColor("#668cff"));
+                    btSelecionado = null;
+                    btVerificar.setClickable(false);
+                    btVerificar.setBackgroundColor(parseColor("#8E8C8C"));
+                }
+            }
+        });
+
+        btQuestao1d.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (btSelecionado != btQuestao1d){
+                    btSelecionado = btQuestao1d;
+
+                    btQuestao1a.setBackgroundColor(parseColor("#668cff"));
+                    btQuestao1b.setBackgroundColor(parseColor("#668cff"));
+                    btQuestao1c.setBackgroundColor(parseColor("#668cff"));
+                    btQuestao1d.setBackgroundColor(parseColor("#66e0ff"));
+
+                    btVerificar.setClickable(true);
+                    btVerificar.setBackgroundColor(parseColor("#bfff00"));
+                }
+                else{
+                    btQuestao1d.setBackgroundColor(parseColor("#668cff"));
+                    btSelecionado = null;
+                    btVerificar.setClickable(false);
+                    btVerificar.setBackgroundColor(parseColor("#8E8C8C"));
+                }
+            }
+        });
+
+        btVerificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(btVerificar.getText().equals("Verificar")) {
+                    verificarResp((String) btSelecionado.getText());
+                    btQuestao1a.setClickable(false);
+                    btQuestao1b.setClickable(false);
+                    btQuestao1c.setClickable(false);
+                    btQuestao1d.setClickable(false);
+                }
+                else{
+                    proxPergunta();
+                    btSelecionado = null;
+                    btVerificar.setClickable(false);
+                    btVerificar.setText("Verificar");
+                    btVerificar.setBackgroundColor(parseColor("#8E8C8C"));
+                    btQuestao1a.setBackgroundColor(parseColor("#668cff"));
+                    btQuestao1b.setBackgroundColor(parseColor("#668cff"));
+                    btQuestao1c.setBackgroundColor(parseColor("#668cff"));
+                    btQuestao1d.setBackgroundColor(parseColor("#668cff"));
+
+                    btQuestao1a.setClickable(true);
+                    btQuestao1b.setClickable(true);
+                    btQuestao1c.setClickable(true);
+                    btQuestao1d.setClickable(true);
+
+                }
+
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -53,7 +191,6 @@ public class TesteSqlActivity extends AppCompatActivity {
     }
 
     private void carregarPerguntas(){
-        ArrayList<String> linhas = new ArrayList<>();
         try {
             InputStream inputStream = getResources().openRawResource(R.raw.perguntassql);
 
@@ -68,31 +205,50 @@ public class TesteSqlActivity extends AppCompatActivity {
             bufferedReader.close();
             inputStream.close();
 
+
+            ordem[0] = -1;
+            for (int i = 0; i < 30; i++){
+                Random random = new Random();
+                int num = random.nextInt(30);
+
+                int diferente = 0;
+
+                for (int j = 0; j < i; j++){
+                    if (ordem[j] != num){
+                        diferente++;
+                    }
+                }
+
+                if (diferente >= i){
+                    ordem[i] = num;
+                }
+                else{
+                    i--;
+                }
+
+            }
+
+
         } catch (Exception e) {
             Toast.makeText(TesteSqlActivity.this, "Erro", Toast.LENGTH_SHORT).show();
         }
 
-        if (!linhas.isEmpty()){
-            /*String[] perguntas = new String[10];
-            Random random = new Random();
+        proxPergunta();
+    }
 
-            Set ordem = new TreeSet();
-            for (int i = 0; i < 3; i++){
-                ordem.add(random.nextInt(3));
+    private void proxPergunta(){
+        if (!linhas.isEmpty()){
+            if (numQuestao < 10){
+                String pergunta = linhas.get(ordem[numQuestao]);
+                exibirPergunta(pergunta);
+                numQuestao++;
+            }
+            else{
+                Toast.makeText(TesteSqlActivity.this, "Teste Terminado", Toast.LENGTH_SHORT).show();
+                finish();
             }
 
-            for(int i = 0; i < 3; i++){
-                perguntas[i] = linhas.get((Integer) ordem.toArray()[i]);
-                questaoNum = i;
-                exibirPergunta(perguntas[i]);
-            }*/
-            String pergunta = linhas.get(numQuestao);
-            exibirPergunta(pergunta);
-            numQuestao++;
-
-
         }
-
     }
     private void exibirPergunta(String linha){
         String[] partes = linha.split(";");
@@ -113,22 +269,40 @@ public class TesteSqlActivity extends AppCompatActivity {
             btQuestao1b.setText(respB);
             btQuestao1c.setText(respC);
             btQuestao1d.setText(respD);
-
+            /*
             btQuestao1a.setOnClickListener(v -> verificarResp(btQuestao1a.getText().toString(), btQuestao1a.getId()));
             btQuestao1b.setOnClickListener(v -> verificarResp(btQuestao1b.getText().toString(), btQuestao1b.getId()));
             btQuestao1c.setOnClickListener(v -> verificarResp(btQuestao1c.getText().toString(), btQuestao1c.getId()));
-            btQuestao1d.setOnClickListener(v -> verificarResp(btQuestao1d.getText().toString(), btQuestao1d.getId()));
+            btQuestao1d.setOnClickListener(v -> verificarResp(btQuestao1d.getText().toString(), btQuestao1d.getId()));*/
         }
     }
 
-    private void verificarResp(String repostaEsc, int btEsc){
+    private void verificarResp(String repostaEsc){
         if (repostaEsc.equals(respCerta)){
             Toast.makeText(TesteSqlActivity.this, "Acertou!", Toast.LENGTH_SHORT).show();
-
+            btVerificar.setBackgroundColor(parseColor("#80ff00"));
+            btSelecionado.setBackgroundColor(parseColor("#80ff00"));
         }
         else{
             Toast.makeText(TesteSqlActivity.this, "Errou!", Toast.LENGTH_SHORT).show();
+
+            btVerificar.setBackgroundColor(parseColor("#ff0000"));
+            btSelecionado.setBackgroundColor(parseColor("#ff0000"));
+
+            if(btQuestao1a != btSelecionado && btQuestao1a.getText().equals(respCerta)){
+                btQuestao1a.setBackgroundColor(parseColor("#80ff00"));
+            }
+            else if(btQuestao1b != btSelecionado && btQuestao1b.getText().equals(respCerta)){
+                btQuestao1b.setBackgroundColor(parseColor("#80ff00"));
+            }
+            else if(btQuestao1c != btSelecionado && btQuestao1c.getText().equals(respCerta)){
+                btQuestao1c.setBackgroundColor(parseColor("#80ff00"));
+            }
+            else if(btQuestao1d != btSelecionado && btQuestao1d.getText().equals(respCerta)){
+                btQuestao1d.setBackgroundColor(parseColor("#80ff00"));
+            }
         }
+        btVerificar.setText("Próxima");
     }
 
 }
