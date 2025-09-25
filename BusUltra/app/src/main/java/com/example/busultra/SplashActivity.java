a49077699a9404df1136326c0326d3d034055df5
Dechomp <julio.sougosan@gmail.com>
2025-09-25 20:27:23 -0300

    Criando as telas do app BusUltra

---
+++ BusUltra/app/src/main/java/com/example/busultra/SplashActivity.java
@@ -0,0 +1,59 @@
+package com.example.busultra;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.webkit.WebView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+    WebView imgGif;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        //Estou relacionando o minha variavel a um componenete na interface
+        imgGif = findViewById(R.id.imgGif);
+
+        imgGif.getSettings().setLoadWithOverviewMode(true);
+        imgGif.getSettings().setUseWideViewPort(true);
+        imgGif.setInitialScale(1);
+
+        imgGif.loadUrl("file:///android_res/drawable/gamestudandologo.gif");
+
+
+        Handler h = new Handler();
+
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                exibirMainActivy();
+
+            }
+        }, 6300);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    private void exibirMainActivy(){
+        Intent telaInicial;
+
+        telaInicial = new Intent(SplashActivity.this, MainActivity.class);
+
+
+        startActivity(telaInicial);
+        finish();
+    }
+}
