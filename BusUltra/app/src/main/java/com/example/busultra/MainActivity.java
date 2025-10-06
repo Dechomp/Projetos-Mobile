c098c5b60a8c0ffe33503ba6a35a6469c7d5f39b
Dechomptile <juliodechomp@gmail.com>
2025-10-06 17:22:45 -0300

    Tentando receber a data no calendarView

--- BusUltra/app/src/main/java/com/example/busultra/MainActivity.java
+++ BusUltra/app/src/main/java/com/example/busultra/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.busultra;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +10,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
+
+    CalendarView cvDataEscolhida;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        cvDataEscolhida = findViewById(R.id.cvDataEscolhida);
+
+
+        //Toast.makeText(MainActivity.this, "Data escolhida: " + dataEsc, Toast.LENGTH_SHORT).show();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
