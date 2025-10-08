4b622ff2a0b2df844efc4e90fafe0cdec09c71b9
Dechomptile <juliodechomp@gmail.com>
2025-10-08 13:12:32 -0300

    Implementando um calendário flutuante

--- BusUltra/app/src/main/java/com/example/busultra/MainActivity.java
+++ BusUltra/app/src/main/java/com/example/busultra/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.busultra;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -16,15 +21,38 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity {
 
 
-    CalendarView cvDataEscolhida;
+    Button btEscolherData;
+
+    EditText edDataViagem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        cvDataEscolhida = findViewById(R.id.cvDataEscolhida);
+       btEscolherData = findViewById(R.id.btEscolherData);
 
+       edDataViagem = findViewById(R.id.edDataViagem);
+
+       btEscolherData.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Calendar calendario = Calendar.getInstance();
+
+               int dia, mes, ano;
+
+               dia = calendario.get(Calendar.DAY_OF_MONTH);
+               mes = calendario.get(Calendar.MONTH);
+               ano = calendario.get(Calendar.YEAR);
+
+               DatePickerDialog calendarioFlutuante = new DatePickerDialog(
+                       MainActivity.this,
+                       ((view, year, month, dayOfMonth) -> {
+                           String dataEsc = dayOfMonth + "/" + (month + 1) + "/" + year;
+                       })
+               );
+           }
+       });
 
         //Toast.makeText(MainActivity.this, "Data escolhida: " + dataEsc, Toast.LENGTH_SHORT).show();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
